5cf2208468662390bd52204bb0751a17ffd28fb0
danilojoncic <djoncic14122rn@raf.rs>
2025-08-08 19:04:19 +0200

    tickers and comarison

--- scripts/pandas_comparison.py
+++ scripts/pandas_comparison.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+import yfinance as yf
+import json
 
 
 df_original = pd.read_csv("/Users/danny/PycharmProjects/CompFinProject/datasets/stock_data_master_20150811_20250808.csv")
 df_cleaned = pd.read_csv("/Users/danny/PycharmProjects/CompFinProject/datasets/without_nulls_stock_data.csv")
-
 
 print(df_cleaned.head())
 print(df_original.head())
@@ -12,7 +12,6 @@ print(df_original.head())
 original_cols = set(df_original.columns)
 cleaned_cols = set(df_cleaned.columns)
 
-# Find which columns were removed
 deleted_cols = original_cols - cleaned_cols
 
 print("Deleted columns (those that had nulls):")
@@ -22,3 +21,29 @@ deleted_tickers = {col.split('_')[0] for col in deleted_cols}
 
 print("Tickers that had columns with nulls (i.e., deleted):")
 print(sorted(deleted_tickers))
+
+sugary_df = pd.read_csv("/Users/danny/PycharmProjects/CompFinProject/datasets/stock_summary_20150811_20250808.csv")
+
+filtered_sugary_df = sugary_df[~sugary_df['Ime'].isin(deleted_tickers)]
+
+filtered_sugary_df.to_csv("cleaned_summary.csv", index=False)
+print("Filtered summary saved.")
+
+
+tickers = sorted(filtered_sugary_df["Ime"].tolist())
+ticker_company = {}
+for ticker in tickers:
+    try:
+        info = yf.Ticker(ticker).info
+        ticker_company[ticker] = info.get("shortName", "Unknown")
+    except:
+        ticker_company[ticker] = "Unknown"
+
+# Save to JSON
+with open("../datasets/ticker_for_portfolio.json", "w") as f:
+    json.dump(ticker_company, f, indent=4)
+
+print("Tickers with names saved.")
+
+
+
