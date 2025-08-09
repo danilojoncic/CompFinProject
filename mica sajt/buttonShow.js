328f9987914df82f07b1ce7cd6b9bbad7cea6818
dzimi23 <dimitrijerajcic45@gmail.com>
2025-08-09 13:04:29 +0200

    Add files via upload

---
+++ mica sajt/buttonShow.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("show-image-button");
+const image = document.getElementById("image1");
+
+button.addEventListener("click", () => {
+  image.style.display = "block";
+});
